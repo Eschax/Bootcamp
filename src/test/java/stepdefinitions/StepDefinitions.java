16918931d2348cf3457f3dd96263e9cea6598092
Eschax <treschaaa@gmail.com>
2025-02-25 23:01:53 +0700

    tugas cucumber selesai

--- src/test/java/stepdefinitions/StepDefinitions.java
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -13,6 +13,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import resources.DataRequest;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tugasqa.model.ResponseObject;
 import com.tugasqa.model.request.RequestItem;
@@ -52,7 +53,8 @@ public class StepDefinitions {
 
     @When("I add item to list {string}")
     public void addItem(String payload) throws JsonProcessingException, com.fasterxml.jackson.core.JsonProcessingException {
-    
+    System.out.println("ini when pake parameter");
+
     RestAssured.baseURI = "https://api.restful-api.dev";
     RequestSpecification requestSpecification = RestAssured
                                                 .given();
@@ -77,7 +79,7 @@ public class StepDefinitions {
 
     // Ambil ID dari respons
     JsonPath addJsonPath = response.jsonPath();
-    objectId = addJsonPath.getString("id"); // Simpan ID untuk digunakan di Then
+    objectId = addJsonPath.getString("id"); 
     
     // Validasi respons
     ResponseObject = addJsonPath.getObject("", ResponseObject.class);
@@ -89,42 +91,67 @@ public class StepDefinitions {
     Assert.assertEquals(ResponseObject.data.price, requestItem.data.price);
     Assert.assertEquals(ResponseObject.data.cpuModel, requestItem.data.cpuModel);
 
-    idProduct = objectId; 
+}
 
-    }
-    @Then("The item is available {string}")
-public void the_item_is_available(String payload) throws JsonProcessingException {
-    System.out.println("ini then");
+    @When("I add item to list")
+    public void addItem() throws JsonProcessingException, JsonMappingException, com.fasterxml.jackson.core.JsonProcessingException {
+    RestAssured.baseURI = "https://api.restful-api.dev";
+    System.out.println("ini When tanpa parameter");
+    RequestSpecification requestSpecification = RestAssured.given();
+
+    // Ambil payload default
+    json = DataRequest.addItemColletion().values().iterator().next();
+
+    Response response = requestSpecification
+        .log()
+        .all()
+        .pathParam("path", "objects")
+        .body(json)
+        .contentType("application/json")
+        .when()
+        .post("{path}");
+
+    ObjectMapper requestAddItem = new ObjectMapper();
+    requestItem = requestAddItem.readValue(json, RequestItem.class);
+
+    // Ambil ID dari respons
+    JsonPath addJsonPath = response.jsonPath();
+    objectId = addJsonPath.getString("id");
+
+    // Validasi respons
+    ResponseObject = addJsonPath.getObject("", ResponseObject.class);
+
+    // Validasi data
+    Assert.assertEquals(response.getStatusCode(), 200, "Status code");
+    Assert.assertEquals(ResponseObject.name, requestItem.name);
+    Assert.assertEquals(ResponseObject.data.year, requestItem.data.year);
+    Assert.assertEquals(ResponseObject.data.price, requestItem.data.price);
+    Assert.assertEquals(ResponseObject.data.cpuModel, requestItem.data.cpuModel);
+}
+
+    @Then("The item is available")
+    public void the_item_is_available() throws JsonProcessingException {
+    System.out.println("ini then tanpa parameter");
 
     RestAssured.baseURI = "https://api.restful-api.dev";
     RequestSpecification requestSpecification = RestAssured.given();
 
-    String json = "";
-    for (Map.Entry<String, String> entry : DataRequest.addItemColletion().entrySet()) {
-        if (entry.getKey().equals(payload)) {
-            json = entry.getValue();
-            break;
-        }
-    }
-
     Response response = requestSpecification
-                            .log()
-                            .all()
-                            .pathParam("path", "objects")
-                            .pathParam("idObject", idProduct)
-                            .body(json)
-                            .contentType("application/json")
-                        .when()
-                        .get("{path}/{idObject}");
+        .log()
+        .all()
+        .pathParam("path", "objects")
+        .pathParam("idObject", objectId)
+        .contentType("application/json")
+        .when()
+        .get("{path}/{idObject}");
 
     JsonPath jsonPath = response.jsonPath();
-
     ResponseObject responseObject = jsonPath.getObject("", ResponseObject.class);
 
     Assert.assertEquals(responseObject.name, requestItem.name);
     Assert.assertEquals(responseObject.data.year, requestItem.data.year);
     Assert.assertEquals(responseObject.data.price, requestItem.data.price);
     Assert.assertEquals(responseObject.data.cpuModel, requestItem.data.cpuModel);
-}
+    }
 
 }
--- target/test-classes/features/ScenarioProducts.feature
+++ target/test-classes/features/ScenarioProducts.feature
@@ -8,7 +8,7 @@ Scenario: As a user I can add new data
 Scenario Outline: As a user I can add new data with different payload
     Given A list of item are available
     When I add item to list "<payload>"
-    Then The item is available "<payload>"
+    Then The item is available
 
     Examples:
     |payload   |
